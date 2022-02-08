63e304b37db0b50d984e8b7a878b6c56fbecd7aa
Michael Salihi <admin@prestance-informatique.fr>
2022-02-08 18:07:20 +0100

    prompt for player name

--- clojurequiz.cljs
+++ clojurequiz.cljs
@@ -3,7 +3,10 @@
    [promesa.core :as p]
    ["chalk$default" :as chalk]
    ["chalk-animation$default" :refer [rainbow]]
+   ["inquirer$default" :as inquirer]
    ["console$log" :as log]))
+
+(def player-name (atom ""))
 
 (defn sleep
   ([] (sleep 2000))
@@ -19,4 +22,13 @@
               "If you get any question wrong I will be " (chalk/bgRed "killed\n")
               "So get all the questions right..."))))
 
-(p/do! (welcome))
+(defn ask-name []
+  (p/let [answers (inquirer/prompt #js {:name "player_name"
+                                        :type "input"
+                                        :message "What is your name?"
+                                        :default (fn [] "Player")})]
+    (reset! player-name (.-player_name answers))
+    (log @player-name)))
+
+(p/do! (welcome)
+       (ask-name))
