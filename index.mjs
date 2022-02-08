94ba1ce24c2d6f45f5f8191bcf20777ac516a9b3
Michael Salihi <admin@prestance-informatique.fr>
2022-02-08 18:38:14 +0100

    update js wrapper to manage path

--- index.mjs
+++ index.mjs
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
 import { loadFile } from 'nbb';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-await loadFile('./clojurequiz.cljs');
+const __dirname = fileURLToPath(dirname(import.meta.url));
+
+await loadFile(resolve(__dirname, 'clojurequiz.cljs'));
